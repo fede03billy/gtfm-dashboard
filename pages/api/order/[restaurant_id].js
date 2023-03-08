ed30ba55e6939ffc4ccab7cfc5bdf3396a6f2a9b
Federico C <fede03billy@gmail.com>
2023-03-08 18:28:04 +0100

    🚧 Food data is now formatted

--- pages/api/order/[restaurant_id].js
+++ pages/api/order/[restaurant_id].js
@@ -3,6 +3,7 @@
 // Path: pages/api/order/[restaurant_id].js
 
 import Order from '../../../models/order';
+import Food from '../../../models/food';
 import databaseConnection from '../../../util/databaseConnection';
 
 databaseConnection();
@@ -17,7 +18,25 @@ export default async function handler(req, res) {
     done: false,
   });
 
-  // TODO: we should also parse the ordered_food array to get the food name from the food table and add it to the response
+  async function getFoodList(orderedFood) {
+    let foodList = [];
+    for (let i = 0; i < orderedFood.length; i++) {
+      await Food.findById(orderedFood[i], { name: 1, price: 1 }).then(
+        (food) => {
+          // INFO: the food returned by this query is not formatted as a JSON
+          // and the property are not accessible with the dot notation
+          // once in on the client side, we use the normalizeFood function to format the food object
+          // and access its properties to be displayed in the UI
+          foodList.push(food);
+        }
+      );
+    }
+    return foodList;
+  }
+  // swap the orders.ordered_food array with the foodList array for each order, where the foodList array contains the complete food objects
+  for (let i = 0; i < orders.length; i++) {
+    orders[i].ordered_food = await getFoodList(orders[i].ordered_food);
+  }
 
   res.json(orders);
 }
